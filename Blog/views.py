c0cecaf83293e4a771e158f3b232a0d027b1db81
Dante-254 <giciadaniel575@gmail.com>
2024-07-25 14:02:05 +0300

    Bootstrap was added and code as blocked

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+#Dummy Database
+posts = [
+    {
+        'author':'Bruno',
+        'title':'Post 101',
+        'content':'This is my head speaking',
+        'date':'July, 25 2024'
+    },
+    {
+        'author':'Manoti',
+        'title':'Post 102',
+        'content':'I present to you my soul',
+        'date':'july, 13 2024'
+    }
+]
 # Create your views here.
 def blog_home(request):
-    return render(request, 'blog_home.html')
+    b_context = {
+        'posts':posts
+    }
+    return render(request, 'blog_home.html', b_context)
