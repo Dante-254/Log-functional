5df8b2d520cc8278c11c5a39b909ec7f490abb88
Dante-254 <giciadaniel575@gmail.com>
2025-05-04 20:01:48 +0300

    /

---
+++ collect_static/frontend_react/src/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+interface buttonProps {
+  children: ReactNode;
+  color?: string;
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: buttonProps) => {
+  return (
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- frontendapp/static/frontend_react/src/App.js
+++ frontendapp/static/frontend_react/src/App.js
@@ -3,12 +3,22 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
+import Alert from "./Alert.tsx";
+import Button from "./Button.tsx";
+import { useState } from "react";
 
 const App = () => {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
-    <div className="container mt-5">
-      <h1 className="text-primary">Hello Bootstrap + React!</h1>
-      <button className="btn btn-success">Click Me</button>
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          <h1>Hello World</h1>
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>
+        <h3>Log In</h3>
+      </Button>
     </div>
   );
 };
