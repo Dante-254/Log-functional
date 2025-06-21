b063b176e8646369944cc4b77d11caf48b54c077
Dante-254 <giciadaniel575@gmail.com>
2025-06-21 10:25:38 +0300

    deletion of frontendapp

---
+++ log/static/frontend_react/src/Alert.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ReactNode } from "react";
+
+interface alertProps {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+const Alert = ({ children, onClose }: alertProps) => {
+  return (
+    <div className="alert alert-info alert-dismissible fade show">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
+
+export default Alert;
